af667f1fff9178b45212f4eeec5ae2d541540f0e
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-09-10 06:09:37 -0400

    Update: added new route for Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import Home from './components/pages/Home';
+import pages from '../src/components/utils/pages';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        
+      <Routes>
+          <Route index element={<Home />} />
+          <Route path={pages.get('home').path} element={<Home />} />
+      </Routes>
       </main>
       <Footer />
       </>
