c21b6655feca4e7362f9923d9d6eb41c413c13c8
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-09-14 16:00:51 -0400

    Update: updated the styles for the pages and reservation, and updated the route for reservation confirmation

--- src/App.js
+++ src/App.js
@@ -3,20 +3,38 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from './components/pages/Home';
 import pages from '../src/components/utils/pages';
+import About from "./components/pages/Home/About";
+import Reservation from "./components/pages/Reservations/Reservation";
+import ConfirmedReservation from './components/pages/Reservations/ConfirmedReservation';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-      <Routes>
+        <Routes>
           <Route index element={<Home />} />
-          <Route path={pages.get('home').path} element={<Home />} />
-      </Routes>
+          <Route
+            path={pages.get('home').path}
+            element={<Home />}
+          />
+          <Route
+            path={pages.get('about').path}
+            element={<About />}
+          />
+          <Route
+            path={pages.get('reservation').path}
+            element={<Reservation />}
+          />
+          <Route
+            path={pages.get('confirmedReservation').path}
+            element={<ConfirmedReservation />}
+          />
+        </Routes>
       </main>
       <Footer />
-      </>
-  
+    </>
+
   );
 }
 
