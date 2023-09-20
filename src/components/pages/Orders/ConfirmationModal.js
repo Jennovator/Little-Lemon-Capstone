1f905681d523d813aed72455808caad20a184c2a
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-09-19 23:00:35 -0400

    Add: added a modal for confirming that the order is added and ask user if they want to view the orders

---
+++ src/components/pages/Orders/ConfirmationModal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Menu.css';
+
+const ConfirmationModal = ({ onConfirm, onCancel }) => {
+  return (
+    <div className="modal-backdrop">
+      <div className="container confirmation-modal">
+        <p>Item added to order. Do you want to view your order?</p>
+        <button onClick={onConfirm}>Yes</button>
+        <button onClick={onCancel}>No</button>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationModal;
