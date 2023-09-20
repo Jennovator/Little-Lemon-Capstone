4cd45a4f8cc37536259931039b91f834557d2471
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-09-19 23:04:17 -0400

    Update: updated the login with maintenance information and added basic styles

---
+++ src/components/pages/Login.js
@@ -0,0 +1,13 @@
+import { faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Login.css';
+const Login = () => {
+  return (
+    <div className="container under-construction">
+      <FontAwesomeIcon icon={faScrewdriverWrench} size='5x'/>
+      <h2>Page under construction</h2>
+    </div>
+  );
+};
+
+export default Login;
