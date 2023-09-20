bfbe2726e290c793495ff5cc4cf7fcf8d6768a57
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-09-19 23:05:53 -0400

    Update: updated the routing for the root component and added the order context for maintaining state through different routes

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import pages from '../src/components/utils/pages';
 import About from "./components/pages/Home/About";
 import Reservation from "./components/pages/Reservations/Reservation";
 import ConfirmedReservation from './components/pages/Reservations/ConfirmedReservation';
+import Menu from './components/pages/Orders/Menu';
+import OrderOnline from './components/pages/Orders/OrderOnline';
+import Login from './components/pages/Login';
+import { OrderProvider } from './provider/OrderContext';
 
 function App() {
   return (
-    <>
+    <OrderProvider>
       <Header />
       <main>
         <Routes>
@@ -30,10 +34,22 @@ function App() {
             path={pages.get('confirmedReservation').path}
             element={<ConfirmedReservation />}
           />
+          <Route
+            path={pages.get('menu').path}
+            element={<Menu />}
+          />
+          <Route
+            path={pages.get('orderOnline').path}
+            element={<OrderOnline />}
+          />
+          <Route
+            path={pages.get('login').path}
+            element={<Login />}
+          />
         </Routes>
       </main>
       <Footer />
-    </>
+    </OrderProvider>
 
   );
 }
