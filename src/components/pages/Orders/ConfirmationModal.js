57806c92851c6d56e5b9a65d5ff5c62972d6ffd2
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-09-20 13:35:55 -0400

    Update: updated the modal with test-id for testing

--- src/components/pages/Orders/ConfirmationModal.js
+++ src/components/pages/Orders/ConfirmationModal.js
@@ -4,7 +4,7 @@ import './Menu.css';
 const ConfirmationModal = ({ onConfirm, onCancel }) => {
   return (
     <div className="modal-backdrop">
-      <div className="container confirmation-modal">
+      <div className="container confirmation-modal" data-testid="confirmation-modal">
         <p>Item added to order. Do you want to view your order?</p>
         <button onClick={onConfirm}>Yes</button>
         <button onClick={onCancel}>No</button>
